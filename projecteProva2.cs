57ad268b50b256982185caad1e8243e7e1cd1a0b
AngelF <angel-abraham.flores.7e8@itb.cat>
2024-12-05 13:45:27 +0100

    Exercic4. Closes #7

--- projecteProva2.cs
+++ projecteProva2.cs
@@ -1,22 +1,43 @@
 ﻿using System;
 using Prova;
-
+using static System.Net.Mime.MediaTypeNames;
+//PRE: rep 3 números	
 namespace projecteProva2
 {
-    public class MyProject
+    public class ValidateNum
     {
         public static void main()
         {
-            const string MSG = "La llista de valors ordeana és: ";
-            const string Error = "Error de programa ";
-            int[] arr = { 10, -4, 6, 4, 8, 13, 2, -4 };
-            int num, low = 0, high = 7;
+            const string Dia = "Introdueix el dia";
+            const string Mes = "Introdueix el mes";
+            const string Any = "Introdueix el any";
+            const string Error = "Erro de programa";
+            int dia, mes, any, totalDaysMonth;
+
             try
             {
-                SecondSort.Order(arr, low, high);
-                for (int i = 0; i < arr.Length; i++)
+                Console.WriteLine(Dia);
+                dia = Convert.ToInt32(Console.ReadLine());
+                if (dia < 1 || dia > 31)
                 {
-                    Console.Write("{0} ", arr[i]);
+                    Console.WriteLine(Mes);
+                    mes = Convert.ToInt32(Console.ReadLine());
+                    if (mes < 1 || mes > 12)
+                    {
+                        if (mes == 1 || mes == 3 || mes == 5 || mes == 7 || mes == 8 || mes == 10 || mes == 12)
+                        {
+                            totalDaysMonth = 31;
+                        }
+                        else
+                            totalDaysMonth = 30;
+                    }
+                    Console.WriteLine(Any);
+                    any = Convert.ToInt32(Console.ReadLine());
+                    if ((any % 400 == 0) || ((any % 4 == 0) && (any % 100 != 0)))
+                    {
+                        totalDaysMonth = 29;
+                    }
+                    else totalDaysMonth = 28;
                 }
             }
             catch (FormatException)
@@ -26,3 +47,4 @@ namespace projecteProva2
         }
     }
 }
+//POST: retorna si els números (dd, mm, yyyy) estan dins del rang de data corresponent
