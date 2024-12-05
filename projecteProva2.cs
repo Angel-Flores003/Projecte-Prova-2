ebb51620bc3c72c60f791b43ae2e2418f0cdc650
AngelF <angel-abraham.flores.7e8@itb.cat>
2024-12-05 14:00:03 +0100

    Exercici5. Closes #9

--- projecteProva2.cs
+++ projecteProva2.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Prova;
 using static System.Net.Mime.MediaTypeNames;
 //PRE: rep 3 números	
@@ -6,39 +7,53 @@ namespace projecteProva2
 {
     public class ValidateNum
     {
+        public static void InsertNum(int[] arr)
+        {
+            const string Prime = "Introdueix 20 números enetrs";
+            
+            for (int i = 0; i < arr.Length; i++)
+            { 
+                Console.WriteLine(Prime);
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+        public static int ValidateNumArr(int[] arr, int first, int last, int key)
+        {
+            if (last >= first)
+            {
+                int mid = first + (last - first) / 2;
+                if (arr[mid] == key)
+                {
+                    return mid;
+                }
+                if (arr[mid] > key)
+                {
+                    return ValidateNumArr(arr, first, mid - 1, key);//cerca en el subarray esquerre
+                }
+                else
+                {
+                    return ValidateNumArr(arr, mid + 1, last, key);//cerca en el subarray dret
+                }
+            }
+            return -1;
+        }
         public static void main()
         {
-            const string Dia = "Introdueix el dia";
+            
             const string Mes = "Introdueix el mes";
             const string Any = "Introdueix el any";
+            const string Num = "Introdueix el número que vols verificas si està a l'array";
             const string Error = "Erro de programa";
-            int dia, mes, any, totalDaysMonth;
+            int first = 0, last = 19, key;
+            int[] arr = new int [20];
 
             try
             {
-                Console.WriteLine(Dia);
-                dia = Convert.ToInt32(Console.ReadLine());
-                if (dia < 1 || dia > 31)
-                {
-                    Console.WriteLine(Mes);
-                    mes = Convert.ToInt32(Console.ReadLine());
-                    if (mes < 1 || mes > 12)
-                    {
-                        if (mes == 1 || mes == 3 || mes == 5 || mes == 7 || mes == 8 || mes == 10 || mes == 12)
-                        {
-                            totalDaysMonth = 31;
-                        }
-                        else
-                            totalDaysMonth = 30;
-                    }
-                    Console.WriteLine(Any);
-                    any = Convert.ToInt32(Console.ReadLine());
-                    if ((any % 400 == 0) || ((any % 4 == 0) && (any % 100 != 0)))
-                    {
-                        totalDaysMonth = 29;
-                    }
-                    else totalDaysMonth = 28;
-                }
+                InsertNum(arr);
+                Console.WriteLine(Num);
+                key = Convert.ToInt32(Console.ReadLine());
+                key = ValidateNumArr(arr, first, last, key);
+
             }
             catch (FormatException)
             {
